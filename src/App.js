bc9dbb454d601174e6e53caaebe4af7917d5c5b5
Daniel Oloyede <oloyedeod@gmail.com>
2023-08-24 19:34:16 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+
+  const [quote, setQuote] = useState('')
+  const [author, setAuthor] = useState('')
+
+useEffect(() => {
+  fetch("http://api.quotable.io/random")
+  .then( res => res.json())
+  .then(
+    (quote) => {
+      setQuote(quote.content)
+      setAuthor(quote.author)
+    }
+  )
+},[]);
+
+let fetchNewQuote = () => {
+  fetch("http://api.quotable.io/random")
+  .then( res => res.json())
+  .then(
+    (quote) => {
+      setQuote(quote.content)
+      setAuthor(quote.author)
+    }
+  )
+}
+
+
+  return (
+    <div className="App">
+      <div className="quote">
+      <h1>{quote}</h1> 
+      <p>{author}</p>
+      <button className="btn" onClick={fetchNewQuote}>Get new Quote</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
