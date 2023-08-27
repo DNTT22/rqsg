fd6175e9b7340ea29f9e309dec5bcc67927f3833
DNTT22 <140279960+DNTT22@users.noreply.github.com>
2023-08-27 20:00:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [author, setAuthor] = useState('')
 
 useEffect(() => {
-  fetch("http://api.quotable.io/random")
+  fetch("https://api.quotable.io/random")
   .then( res => res.json())
   .then(
     (quote) => {
@@ -18,7 +18,7 @@ useEffect(() => {
 },[]);
 
 let fetchNewQuote = () => {
-  fetch("http://api.quotable.io/random")
+  fetch("https://api.quotable.io/random")
   .then( res => res.json())
   .then(
     (quote) => {
